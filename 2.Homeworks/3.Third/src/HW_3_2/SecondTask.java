10a4c588d15639f8167a33d75288b7decfbbe498
Echolz <elizaremanuilov@gmail.com>
2018-10-22 11:43:34 +0300

    modified second task

--- 2.Homeworks/3.Third/src/HW_3_2/SecondTask.java
+++ 2.Homeworks/3.Third/src/HW_3_2/SecondTask.java
@@ -1,85 +1,74 @@
 package HW_3_2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class SecondTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         StringBuilder answer = new StringBuilder();
 
         int n = scanner.nextInt();
 
-        List<Long> numbers = new ArrayList<>();
+        List<Long> numbers = new ArrayList<>(n);
 
         for (int i = 0; i < n; i++) {
             numbers.add(scanner.nextLong());
         }
 
+        Collections.sort(numbers);
+
         int q = scanner.nextInt();
 
-        long p;
-        int currentIndex;
-        int lowestIndex;
-        int highestIndex;
+        long number;
+
         for (int i = 0; i < q; i++) {
-            p = scanner.nextLong();
+            number = scanner.nextLong();
 
-            currentIndex = binarySearch(numbers, 0, numbers.size() - 1, p);
-
-            if (currentIndex == -1) {
-                answer.append("-1 -1\n");
-                continue;
-            }
-            lowestIndex = findLowestIndex(numbers, currentIndex);
-            highestIndex = findHighestIndex(numbers, currentIndex);
-            answer.append(lowestIndex + 1).append(" ").append(highestIndex + 1).append("\n");
+            answer.append(binarySearchMinIndex(numbers, 0, numbers.size() - 1, number, -2) + 1)
+                    .append(" ")
+                    .append(binarySearchMaxIndex(numbers, 0, numbers.size() - 1, number, -2) + 1)
+                    .append("\n");
         }
 
         System.out.println(answer.toString());
     }
 
-
-    static int binarySearch(List<Long> numbers, int left, int right, long numberToFind) {
+    static int binarySearchMinIndex(List<Long> numbers, int left, int right, Long number, int minIndex) {
         if (left <= right) {
-            int currentIndex = left + (right - left) / 2;
-            long currentNumber = numbers.get(currentIndex);
+            int mid = (left + right) / 2;
 
-            if (currentNumber == numberToFind) {
-                return currentIndex;
+            if (numbers.get(mid).equals(number)) {
+                return binarySearchMinIndex(numbers, left, mid - 1, number, mid);
             }
 
-            if (currentNumber >= numberToFind) {
-                return binarySearch(numbers, left, currentIndex - 1, numberToFind);
-            } else {
-                return binarySearch(numbers, currentIndex + 1, right, numberToFind);
+            if (number < numbers.get(mid)) {
+                return binarySearchMinIndex(numbers, left, mid - 1, number, minIndex);
             }
+
+            return binarySearchMinIndex(numbers, mid + 1, right, number, minIndex);
         }
 
-        return -1;
+        return minIndex;
     }
 
-    static int findLowestIndex(List<Long> numbers, int currentIndex) {
-        long currentNumber = numbers.get(currentIndex);
-        int indexToReturn = currentIndex;
+    static int binarySearchMaxIndex(List<Long> numbers, int left, int right, long number, int maxIndex) {
+        if (left <= right) {
+            int mid = (left + right) / 2;
 
-        while (indexToReturn - 1 >= 0 && numbers.get(indexToReturn - 1) == currentNumber) {
-            indexToReturn --;
+            if (numbers.get(mid).equals(number)) {
+                return binarySearchMaxIndex(numbers, mid + 1, right, number, mid);
+            }
+
+            if (number < numbers.get(mid)) {
+                return binarySearchMaxIndex(numbers, left, mid - 1, number, maxIndex);
+            }
+
+            return binarySearchMaxIndex(numbers, mid + 1, right, number, maxIndex);
         }
 
-        return indexToReturn;
-    }
-
-    static int findHighestIndex(List<Long> numbers, int currentIndex) {
-        long currentNumber = numbers.get(currentIndex);
-        int indexToReturn = currentIndex;
-
-        while (indexToReturn + 1 <= numbers.size() - 1 && numbers.get(indexToReturn + 1) == currentNumber) {
-            indexToReturn ++;
-        }
-
-        return indexToReturn;
+        return maxIndex;
     }
 }
