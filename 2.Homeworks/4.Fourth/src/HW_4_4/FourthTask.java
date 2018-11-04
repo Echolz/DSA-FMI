426840c4e4fa94dfe0324ebe15cfa8f606d5597d
Echolz <elizaremanuilov@gmail.com>
2018-11-04 15:12:13 +0200

    final push for fourth task

--- 2.Homeworks/4.Fourth/src/HW_4_4/FourthTask.java
+++ 2.Homeworks/4.Fourth/src/HW_4_4/FourthTask.java
@@ -1,21 +1,20 @@
 package HW_4_4;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class FourthTask {
     public static void main(String[] args) {
-        InputReader scanner = new InputReader();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        int n = scanner.readInt();
+        scanner.nextLine();
 
         String[] inputArr;
 
         LinkedList linkedList = new LinkedList();
 
         for (int i = 0; i < n; i++) {
-            inputArr = scanner.readLine().split(" ");
+            inputArr = scanner.nextLine().split(" ");
 
             if (inputArr[0].equals("add")) {
                 linkedList.add(Integer.parseInt(inputArr[1]));
@@ -135,7 +134,12 @@ class LinkedList {
             currentNode = currentNode.right;
         }
 
+        if (sb.length() == 0) {
+            return;
+        }
+
         sb.setLength(sb.length() - 1);
+
 
         System.out.println(sb.toString());
     }
@@ -148,132 +152,5 @@ class Node {
 
     Node(int value) {
         this.value = value;
-    }
-}
-
-class InputReader {
-    private InputStream stream;
-    private byte[] buf = new byte[1024];
-    private int curChar;
-    private int numChars;
-
-    InputReader() {
-        this.stream = System.in;
-    }
-
-    int read() {
-        if (numChars == -1)
-            throw new InputMismatchException();
-        if (curChar >= numChars) {
-            curChar = 0;
-            try {
-                numChars = stream.read(buf);
-            } catch (IOException e) {
-                throw new InputMismatchException();
-            }
-            if (numChars <= 0)
-                return -1;
-        }
-        return buf[curChar++];
-    }
-
-    int readInt() {
-        int c = read();
-        while (isSpaceChar(c)) {
-            c = read();
-        }
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        int res = 0;
-        do {
-            if (c < '0' || c > '9') {
-                throw new InputMismatchException();
-            }
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
-
-    long readLong() {
-        int c = read();
-        while (isSpaceChar(c)) {
-            c = read();
-        }
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        long res = 0;
-        do {
-            if (c < '0' || c > '9') {
-                throw new InputMismatchException();
-            }
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
-
-    double readDouble() {
-        int c = read();
-        while (isSpaceChar(c)) {
-            c = read();
-        }
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        double res = 0;
-        while (!isSpaceChar(c) && c != '.' && c != ',') {
-            if (c == 'e' || c == 'E') {
-                return res * Math.pow(10, readInt());
-            }
-            if (c < '0' || c > '9') {
-                throw new InputMismatchException();
-            }
-            res *= 10;
-            res += c - '0';
-            c = read();
-        }
-        if (c == '.' || c == ',') {
-            c = read();
-            double m = 1;
-            while (!isSpaceChar(c)) {
-                if (c == 'e' || c == 'E') {
-                    return res * Math.pow(10, readInt());
-                }
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                m /= 10;
-                res += (c - '0') * m;
-                c = read();
-            }
-        }
-        return res * sgn;
-    }
-
-    String readLine() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        StringBuilder res = new StringBuilder();
-        do {
-            res.appendCodePoint(c);
-            c = read();
-        } while (!isSpaceChar(c));
-        return res.toString();
-    }
-
-    boolean isSpaceChar(int c) {
-        return c == '\n';
     }
 }
