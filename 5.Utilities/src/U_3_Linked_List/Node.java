29f613e3b44bbbc2ad4e48ea8850c309700e2ce4
Echolz <elizaremanuilov@gmail.com>
2018-11-09 14:33:07 +0200

    added linked list and file writer implementation

---
+++ 5.Utilities/src/U_3_Linked_List/Node.java
@@ -0,0 +1,35 @@
+package U_3_Linked_List;
+
+public class Node<T> {
+    private T value;
+    private Node<T> left;
+    private Node<T> right;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+}
