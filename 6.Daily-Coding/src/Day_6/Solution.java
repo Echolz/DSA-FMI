857b7dc8a98d6bf9adfd37ecd737c7f84f4512d5
Echolz <elizaremanuilov@gmail.com>
2018-11-10 23:44:28 +0200

    added day 6 solution class

--- 6.Daily-Coding/src/Day_6/Solution.java
+++ 6.Daily-Coding/src/Day_6/Solution.java
@@ -1,7 +1,11 @@
 package Day_6;
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
     }
 }
