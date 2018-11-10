c1a667b4128f0b2855506bc96859563a8498fbbb
Echolz <elizaremanuilov@gmail.com>
2018-11-10 23:32:08 +0200

    added file writer to utilities and added notes from fifth exercise

---
+++ 5.Utilities/src/U_4_File_Writer/TextWriter.java
@@ -0,0 +1,47 @@
+package U_4_File_Writer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TextWriter {
+
+    private File fileOut;
+    private FileWriter fileWriterOut;
+    private PrintWriter printWriterOut;
+
+    private File fileIn;
+    private FileWriter fileWriterIn;
+    private PrintWriter printWriterIn;
+
+
+    public TextWriter() throws IOException {
+        fileOut = new File("out.txt");
+        fileWriterOut = new FileWriter(fileOut);
+        printWriterOut = new PrintWriter(fileWriterOut);
+
+
+        fileIn = new File("in.txt");
+        fileWriterIn = new FileWriter(fileIn);
+        printWriterIn = new PrintWriter(fileWriterIn);
+
+    }
+
+    static public int getRandomNumberInRange(int lower, int upper) {
+        return (int) (Math.random() * ((upper - lower) + 1)) + lower;
+    }
+
+    public void printOut(String toPrint) {
+        printWriterOut.println(toPrint);
+    }
+
+    public void printIn(String toPrint) {
+        printWriterIn.println(toPrint);
+    }
+
+    public void closeWriter() {
+        this.printWriterOut.close();
+        this.printWriterIn.close();
+    }
+}
