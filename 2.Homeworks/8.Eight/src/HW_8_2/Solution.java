cf54f40033cf1849eb09accb30ae2379ebbb8fad
Echolz <elizaremanuilov@gmail.com>
2018-12-04 12:53:47 +0200

    did second task of eighth homework

--- 2.Homeworks/8.Eight/src/HW_8_2/Solution.java
+++ 2.Homeworks/8.Eight/src/HW_8_2/Solution.java
@@ -1,11 +1,32 @@
 package HW_8_2;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        Set<Integer> peopleInTeam = new HashSet<>();
+
+        int firstPlayer = scanner.nextInt();
+
+        peopleInTeam.add(firstPlayer);
+
+        int n = scanner.nextInt();
+
+        int answer = 1;
+
+        for (int i = 0; i < n; i++) {
+            int bestFriend = scanner.nextInt();
+            int player = scanner.nextInt();
+
+            if (peopleInTeam.contains(bestFriend)) {
+                peopleInTeam.add(player);
+            }
+        }
+
+        System.out.println(peopleInTeam.size());
     }
 }
