e1125ae26e79c75ebd87690b860a37fc46075175
Echolz <elizaremanuilov@gmail.com>
2018-12-15 01:19:16 +0200

    added day 28 solution 2, not finished

--- 6.Daily-Coding/src/Day_28/Solution2.java
+++ 6.Daily-Coding/src/Day_28/Solution2.java
@@ -1,7 +1,36 @@
 package Day_28;
 
+import java.util.List;
+
 public class Solution2 {
     public static void main(String[] args) {
 
+    }
+
+    static int activityNotifications(int[] expenditure, int d) {
+        int[] buckets = new int[201];
+
+        int answer = 0;
+
+        for (int i = 0; i < expenditure.length; i++) {
+            int number = expenditure[i];
+
+            if (i < d) {
+                buckets[number]++;
+                continue;
+            }
+
+            if (i == 0) {
+                int median = findMedian(buckets);
+            }
+        }
+    }
+
+    private static int findMedian(int[] buckets) {
+        List<Integer>
+
+        for (int i = 0; i < buckets.length; i++) {
+
+        }
     }
 }
