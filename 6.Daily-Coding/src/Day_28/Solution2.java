da31c60a88a878a8ebebd70c990c62f82e18d100
Echolz <elizaremanuilov@gmail.com>
2018-12-17 15:00:20 +0200

    added day 28 second solution

--- 6.Daily-Coding/src/Day_28/Solution2.java
+++ 6.Daily-Coding/src/Day_28/Solution2.java
@@ -1,36 +1,80 @@
 package Day_28;
 
-import java.util.List;
-
 public class Solution2 {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 4, 4, 4};
+        System.out.println(activityNotifications(arr, 2));
     }
 
     static int activityNotifications(int[] expenditure, int d) {
         int[] buckets = new int[201];
-
         int answer = 0;
 
         for (int i = 0; i < expenditure.length; i++) {
             int number = expenditure[i];
 
-            if (i < d) {
-                buckets[number]++;
-                continue;
+            if (i >= d) {
+                int median = findMedian(buckets, d);
+
+                if (median <= number) {
+                    answer++;
+                }
             }
 
-            if (i == 0) {
-                int median = findMedian(buckets);
+            if (i >= d) {
+                buckets[expenditure[i - d]]--;
             }
+
+            buckets[number]++;
+        }
+
+        return answer;
+    }
+
+    static int getRoundedDivision(int n) {
+        if (n % 2 == 1) {
+            return (n / 2) + 1;
+        } else {
+            return n / 2;
         }
     }
 
-    private static int findMedian(int[] buckets) {
-        List<Integer>
+    private static int findMedian(int[] buckets, int d) {
+        int size = 0;
+        if (d % 2 == 1) {
+            int mid = getRoundedDivision(d);
+            int answer = -1;
+            for (int i = 0; i < buckets.length; i++) {
+                size += buckets[i];
+                if (size < mid) {
+                    continue;
+                }
+                answer = i;
+                break;
+            }
 
-        for (int i = 0; i < buckets.length; i++) {
+            return answer * 2;
+        } else {
+            int first = d / 2;
+            int second = (d / 2) + 1;
 
+            int firstNum = -1;
+            int secondNum = -1;
+
+            for (int i = 0; i < buckets.length; i++) {
+                size += buckets[i];
+
+                if (size >= first && firstNum == -1) {
+                    firstNum = i;
+                }
+
+                if (size >= second) {
+                    secondNum = i;
+                    break;
+                }
+            }
+
+            return firstNum + secondNum;
         }
     }
 }
