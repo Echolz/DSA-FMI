2702e77129e5404fbbc5bf8263a80f77e5a22bbc
echolz <elizaremanuilov@gmail.com>
2019-11-11 16:39:57 +0200

    Add fourth task of third homework

--- 7.Second-Year/3.Third/src/HW_3_4/FourthTask.java
+++ 7.Second-Year/3.Third/src/HW_3_4/FourthTask.java
@@ -1,50 +1,64 @@
 package HW_3_4;
 
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Stack;
+import java.util.Set;
 
 public class FourthTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int q = scanner.nextInt();
+        int n = scanner.nextInt();
 
-        Stack<Integer> stack;
-
-        boolean flag = true;
-
-        for (int i = 0; i < q; i++) {
-            int k = scanner.nextInt();
-
-            stack = new Stack<>();
-
-            for (int j = 0; j < k; j++) {
-                int currentTruck = scanner.nextInt();
-
-                if (currentTruck == j - stack.size() + 1) {
-                    continue;
-                }
-
-                if (stack.size() == 0) {
-                    stack.push(currentTruck);
-                    continue;
-                }
-
-                if (stack.peek() < currentTruck) {
-                    flag = false;
-                    continue;
-                }
-
-                stack.push(currentTruck);
-            }
-
-            if (flag) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
+        if (n == 0) {
+            System.out.println(0);
+            return;
         }
 
+        int[] trucks = new int[20001];
 
+        Set<Integer> usedHeights = new HashSet<>();
+
+
+        for (int i = 0; i < n; i++) {
+            int currentTruckDelta = scanner.nextInt();
+            int currentTruckNeededPower = scanner.nextInt();
+            trucks[currentTruckDelta + 10000] += currentTruckNeededPower;
+            usedHeights.add(currentTruckDelta + 10000);
+        }
+
+        Long min = Long.MAX_VALUE;
+        Long current = 0L;
+
+        for (Integer usedHeight : usedHeights) {
+            for (Integer uh : usedHeights) {
+                if (uh == usedHeight) {
+                    continue;
+                }
+                current += Math.abs(usedHeight - uh) * trucks[uh];
+            }
+            if (current < min) {
+                min = current;
+            }
+
+            current = 0L;
+        }
+//        for (int i = 0; i < 20001; i++) {
+//            for (int j = 0; j < 20001; j++) {
+//                if (i == j) {
+//                    continue;
+//                }
+//
+//                current += Math.abs(i - j) * trucks[j];
+//
+//            }
+//            if (current < min) {
+//                min = current;
+//            }
+//
+//            current = 0L;
+//        }
+
+        System.out.println(min);
     }
 }
