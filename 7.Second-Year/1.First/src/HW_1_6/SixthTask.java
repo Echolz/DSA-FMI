fd8934acb7e627cf5e1657d251d6d4f369682eed
echolz <elizaremanuilov@gmail.com>
2019-11-20 16:46:23 +0200

    Add first exam solution

---
+++ 7.Second-Year/1.First/src/HW_1_6/SixthTask.java
@@ -0,0 +1,157 @@
+package HW_1_6;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+
+public class SixthTask {
+    public static void main(String[] args) {
+        InputReader scanner = new InputReader();
+
+        int maxNumber = 100001;
+        int[] arr = new int[maxNumber];
+
+        int n = scanner.readInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[scanner.readInt()]++;
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = maxNumber - 1; i >= 0; i--) {
+            for (int j = 0; j < arr[i]; j++) {
+                answer.append(i).append(" ");
+            }
+        }
+
+        System.out.println(answer.toString());
+    }
+}
+
+class InputReader {
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+
+    InputReader() {
+        this.stream = System.in;
+    }
+
+    int read() {
+        if (numChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    int readInt() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    long readLong() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    double readDouble() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        double res = 0;
+        while (!isSpaceChar(c) && c != '.' && c != ',') {
+            if (c == 'e' || c == 'E') {
+                return res * Math.pow(10, readInt());
+            }
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        }
+        if (c == '.' || c == ',') {
+            c = read();
+            double m = 1;
+            while (!isSpaceChar(c)) {
+                if (c == 'e' || c == 'E') {
+                    return res * Math.pow(10, readInt());
+                }
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                m /= 10;
+                res += (c - '0') * m;
+                c = read();
+            }
+        }
+        return res * sgn;
+    }
+
+    String readLine() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    boolean isSpaceChar(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+}
